d050ea038b05086dae4bccba023c4d20e8714789
Kjoobin <imkj1148@gmail.com>
2020-08-30 17:32:34 +0900

    Add text input for search

--- src/search/SearchBar.js
+++ src/search/SearchBar.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 export default class SearchBar extends Component {
     render() {
         return (
-            <View>
-                <Text> SearchBar </Text>
+            <View style={styles.wrapper}>
+                <TextInput style={styles.input} />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    wrapper: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    input: {
+        height: 35,
+        width: "70%",
+        borderRadius: 50,
+        paddingLeft: 20,
+        backgroundColor: "rgba(125,125,125,0.15)",
+    }
+})
